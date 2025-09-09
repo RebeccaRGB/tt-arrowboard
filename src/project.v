f90d63b336d885eac370cdec9b0e77a085ac2777
RebeccaRGB <beckiejo@gmail.com>
2025-09-09 00:25:06 -0700

    Ignore unused input

--- src/project.v
+++ src/project.v
@@ -48,6 +48,6 @@ module tt_um_rebeccargb_arrow_board (
   assign uio_oe  = 8'hFF;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in, 1'b0};
 
 endmodule
